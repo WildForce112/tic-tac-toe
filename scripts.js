0acd1227a1cbd0ade40141e19c4302e0b3e8e749
WildForce112 <81035327+WildForce112@users.noreply.github.com>
2025-12-03 22:38:02 +0700

    feat: Added players and score display

--- scripts.js
+++ scripts.js
@@ -1,14 +1,36 @@
+
 (function () {
+  const createPlayer = (name, marker, score = 0) => ({name, marker, score});
+
   let board = Array(9).fill('');
   let $board = null;
-  let currentPlayer = 'X';
+  let $player1 = null;
+  let $player2 = null;
+  const player1 = createPlayer('player1', 'X');
+  const player2 = createPlayer('player2', 'O');
+  let currentPlayer = player1.marker;
 
   const init = () => {
     $board = document.querySelector('.board');
+    $player1 = document.querySelector('.player1');
+    $player2 = document.querySelector('.player2');
+
+    console.log($player1);
+
     if (!$board) {
       console.error('.board element not found');
       return;
     }
+    if (!$player1) {
+      console.error('.player1 element not found');
+      return;
+    }
+    if (!$player2) {
+      console.error('.player2 element not found');
+      return;
+    }
+
+
 
     $board.addEventListener('click', (e) => {
       const cell = e.target.closest('.cell');
@@ -26,12 +48,28 @@
     return $cell;
   }
 
+  const renderPlayer = (player, $player) => {
+    $player.textContent = '';
+    const $name = document.createElement('div');
+    const $marker = document.createElement('div');
+    const $score = document.createTextNode(player.score);
+    $name.textContent = `Name: ${player.name}`;
+    $marker.textContent = `Marker: ${player.marker}`;
+
+    $player.appendChild($name);
+    $player.appendChild($marker);
+    $player.appendChild($score);
+  }
+
   const renderBoard = (boardArray) => {
     $board.innerHTML = ''; // clear old board;
     boardArray.forEach((value, index) => {
       const cell = renderCell(index, value);
       $board.appendChild(cell);
     });
+    renderPlayer(player1, $player1);
+    renderPlayer(player2, $player2);
+
   }
 
   const handlePlayerMove = (index) => {
@@ -43,11 +81,13 @@
     const winner = checkWinner(board);
     if (winner) {
       setTimeout(() => alert(`${winner} wins!`), 0);
-      // freeze further input
+      winner === player1.marker ? player1.score++ : player2.score++;
+      reset();
       return;
     }
     if (board.every(Boolean)) {
       setTimeout(() => alert(`Draw!`), 0);
+      reset();
       return;
     }
 
